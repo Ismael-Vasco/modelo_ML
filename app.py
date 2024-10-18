9d09018e3872b45eee91d39f4838086d6c3249fb
Ismael-Vasco <157051386+Ismael-Vasco@users.noreply.github.com>
2024-10-17 19:33:04 -0500

    first commit

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-from flask import Flask
+# pip install flask
+from flask import Flask, render_template, request
+# 
+import pickle
+
+# pip install nltk
+# import nltk
+
+# inicializar app de Flask
+app =  Flask(__name__)
+
+# cargar el modelo
+with open('modelo.pkl', 'rb') as f:
+    modelo_tfdf = pickle.load(f)
